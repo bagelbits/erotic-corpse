c02b5f121dbef0f67e7289ce9c6f5cee57d3a6c5
Christopher Ward <cdurianward@gmail.com>
2020-05-15 21:18:12 -0700

    Rename the rest to Erotic Corpse

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ require 'sprockets/railtie'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module ExquisiteCorpseTelephone
+module EroticCorpse
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
