ca646da21fc529725bd2ac166fa851b1fbe4f711
Christopher Ward <cdurianward@gmail.com>
2020-05-16 11:04:17 -0700

    Some basic React setup

---
+++ app/javascript/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import PropTypes from "prop-types";
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome to Erotic Corpse</h1>
+        <React.Fragment>Look at me! I'm React!</React.Fragment>
+      </div>
+    );
+  }
+}
+
+App.propTypes = {};
+export default App;
