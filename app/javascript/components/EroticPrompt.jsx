5574f73514f835e24572444f4fd7ec3b61b3962f
Christopher Ward <cdurianward@gmail.com>
2020-05-20 01:13:46 -0700

    Wait. Set that back.

--- app/javascript/components/EroticPrompt.jsx
+++ app/javascript/components/EroticPrompt.jsx
@@ -140,7 +140,7 @@ function EroticPrompt(props) {
           <p> {result.prompt} </p>
           <CountdownTimer
             isActive={true}
-            seconds={10}
+            seconds={180}
             onFinish={() => {
               setSubmitted(true);
             }}
